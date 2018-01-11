49f70912e25866dfb96c30e0bf9e06322b7a981c
smile4994 <30884887+smile4994@users.noreply.github.com>
2018-01-11 17:39:24 +0900

    up
    up

--- app/src/main/java/com/example/student/codieshare/Gallery.java
+++ app/src/main/java/com/example/student/codieshare/Gallery.java
@@ -1,7 +1,6 @@
 package com.example.student.codieshare;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,81 +11,39 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.example.student.codieshare.child_fragment.AccFragment;
-import com.example.student.codieshare.child_fragment.BottomFragment;
-import com.example.student.codieshare.child_fragment.ShoesFragment;
-import com.example.student.codieshare.child_fragment.TopFragment;
+import com.example.student.codieshare.gallery_tab.Acc;
+import com.example.student.codieshare.gallery_tab.Bottom;
+import com.example.student.codieshare.gallery_tab.Outer;
+import com.example.student.codieshare.gallery_tab.Shoes;
+import com.example.student.codieshare.gallery_tab.Top;
+import com.soundcloud.android.crop.Crop;
 
 /**
  * Created by student on 2018-01-09.
  */
 
-public class Gallery extends Fragment implements OnClickListener {
+public class Gallery extends Fragment{
+    private Context context;
+    private Button btn_outer, btn_top, btn_bottom, btn_shoes, btn_acc;
 
-    public static Gallery newInstance() {
-        return new Gallery();
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.gallery, container, false);
 
-        View fv = inflater.inflate(R.layout.gallery, container, false);
-        Button btn_outer, btn_top, btn_bottom, btn_shoes, btn_acc;
+        btn_outer = view.findViewById(R.id.btn_outer);
+        btn_top = view.findViewById(R.id.btn_top);
+        btn_bottom = view.findViewById(R.id.btn_bottom);
+        btn_shoes = view.findViewById(R.id.btn_shoes);
+        btn_acc = view.findViewById(R.id.btn_acc);
 
-        btn_outer = (Button) fv.findViewById(R.id.btn_outer);
-        btn_top = (Button) fv.findViewById(R.id.btn_top);
-        btn_bottom = (Button) fv.findViewById(R.id.btn_bottom);
-        btn_shoes = (Button) fv.findViewById(R.id.btn_shoes);
-        btn_acc = (Button) fv.findViewById(R.id.btn_acc);
+        btn_outer.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Crop.pickImage(getContext(), Gallery.this);
+            }
+        });
 
-        return fv;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Fragment fg;
-        switch (v.getId()) {
-            case R.id.btn_outer:
-                fg = TopFragment.newInstance();
-                setChildFragment(fg);
-                break;
-
-            case R.id.btn_top:
-                fg = TopFragment.newInstance();
-                setChildFragment(fg);
-                break;
-
-            case R.id.btn_bottom:
-                fg = BottomFragment.newInstance();
-                setChildFragment(fg);
-                break;
-
-            case R.id.btn_shoes:
-                fg = ShoesFragment.newInstance();
-                setChildFragment(fg);
-                break;
-
-            case R.id.btn_acc:
-                fg = AccFragment.newInstance();
-                setChildFragment(fg);
-                break;
-
-        }
-    }
-
-    private void setChildFragment(Fragment child) {
-        FragmentTransaction childFt = getChildFragmentManager().beginTransaction();
-
-        if (!child.isAdded()) {
-            childFt.replace(R.id.viewpager_child, child);
-            childFt.addToBackStack(null);
-            childFt.commit();
-        }
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/student/codieshare/gallery_tab/Outer.java
@@ -0,0 +1,33 @@
+package com.example.student.codieshare.gallery_tab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.student.codieshare.R;
+
+/**
+ * Created by student on 2018-01-10.
+ */
+
+public class Outer extends Fragment{
+    public static Outer newInstance(){
+        return new Outer();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fv = inflater.inflate(R.layout.outer, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
