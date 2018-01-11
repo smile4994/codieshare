079bfc4f5a0a80ba58429f98009f6f8369502082
tkaks12 <tkaks12@naver.com>
2018-01-11 14:03:16 +0900

    yong
    yot

--- app/src/main/java/com/example/student/codieshare/Picture.java
+++ app/src/main/java/com/example/student/codieshare/Picture.java
@@ -1,48 +1,67 @@
 package com.example.student.codieshare;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
+import java.net.URI;
 
 /**
  * Created by student on 2018-01-09.
  */
 
-public class Picture extends Fragment{
-    private  final int pictureCode = 1;
+public class Picture extends Fragment {
     private Button btnGoPicture;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.picture, container,false);
+        View view = inflater.inflate(R.layout.picture, container, false);
+
         btnGoPicture = view.findViewById(R.id.btn_edit_picture);
         btnGoPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"테스트성공",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getActivity(),PictureActivity.class);
-                startActivityForResult(intent,pictureCode);
+                Crop.pickImage(getContext(), Picture.this);
             }
         });
         return view;
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode, data);
-        if (resultCode == pictureCode){
-            if (resultCode == 200){
-                Toast.makeText(getContext(),"클릭햇지?",Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(getContext(),"뒤로 버튼을 눌렀군",Toast.LENGTH_SHORT).show();
-            }
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == Crop.REQUEST_PICK && resultCode == Activity.RESULT_OK) {
+            beginCrop(result.getData());
+        } else if (requestCode == Crop.REQUEST_CROP) {
+            handleCrop(resultCode, result);
         }
+    }
 
+    private void beginCrop(Uri source) {
+        Uri destination = Uri.fromFile(new File(getContext().getCacheDir(), "cropped"));
+        Crop.of(source, destination).asSquare().start(getContext(), Picture.this);
+    }
+
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == Activity.RESULT_OK) {
+            Intent intent = new Intent(getActivity(), PictureActivity.class);
+            String pictuerUri = Crop.getOutput(result).toString();
+            intent.putExtra("pictuerUri",pictuerUri);
+            startActivity(intent);
+        } else if (resultCode == Crop.RESULT_ERROR) {
+        }
     }
 }
--- app/src/main/java/com/example/student/codieshare/PictureActivity.java
+++ app/src/main/java/com/example/student/codieshare/PictureActivity.java
@@ -1,81 +1,160 @@
 package com.example.student.codieshare;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.soundcloud.android.crop.Crop;
-
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by student on 2018-01-10.
  */
 
 public class PictureActivity extends Activity {
-    private Button apBtnEdit;
+    private Button apBtnSave;
     private ImageView apImageview;
-    private int pressBackCount = 0;
+
+    private Bitmap bm;
+    private String savePictureName;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
 
-        apBtnEdit = findViewById(R.id.ap_btn_edit);
+        apBtnSave = findViewById(R.id.ap_btn_save);
         apImageview = findViewById(R.id.ap_imageview);
 
-        apBtnEdit.setOnClickListener(new View.OnClickListener() {
+        // fragment에서 사진 가져오는 작업
+        Intent intent = new Intent(this.getIntent());
+        String msg = intent.getStringExtra("pictuerUri");
+        Uri pictureUri = Uri.parse(msg);
+        apImageview.setImageDrawable(null);
+        apImageview.setImageURI(pictureUri);
+
+        // Uri를 비트맵으로 저장
+        try {
+            bm = MediaStore.Images.Media.getBitmap(getContentResolver(), pictureUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        apBtnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                apImageview.setImageDrawable(null);
-                Crop.pickImage(PictureActivity.this);
+               makeDialogSaveName().show();
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
-        if (requestCode == Crop.REQUEST_PICK && resultCode == Activity.RESULT_OK) {
-            Log.d("onActivityResult", "request pick");
-            beginCrop(result.getData());
-        } else if (requestCode == Crop.REQUEST_CROP) {
-            Log.d("onActivityResult", "request crop");
-            handleCrop(resultCode, result);
+    private Dialog makeDialogSave() {
+        final Dialog custom = new Dialog(this);
+        custom.setContentView(R.layout.dialog_save);
+        Button outerwear = custom.findViewById(R.id.btn_outerwear);
+        Button shirt = custom.findViewById(R.id.btn_shirt);
+        Button pants = custom.findViewById(R.id.btn_pants);
+        Button shoes = custom.findViewById(R.id.btn_shoes);
+
+        outerwear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImage(bm, "아우터", savePictureName);
+                custom.cancel();
+                Toast.makeText(getApplicationContext(), "아우터 폴터에 저장됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        shirt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImage(bm, "상의", savePictureName);
+                custom.cancel();
+                Toast.makeText(getApplicationContext(), "상의 폴터에 저장됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        pants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImage(bm, "하의", savePictureName);
+                custom.cancel();
+                Toast.makeText(getApplicationContext(), "하의 폴터에 저장됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        shoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImage(bm, "신발", savePictureName);
+                custom.cancel();
+                Toast.makeText(getApplicationContext(), "신발 폴터에 저장됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return custom;
+    }
+
+    private Dialog makeDialogSaveName() {
+        final Dialog custom = new Dialog(this);
+        custom.setContentView(R.layout.dialog_save_name);
+        final EditText name = custom.findViewById(R.id.et_save);
+        Button saveName = custom.findViewById(R.id.btn_save);
+
+        saveName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePictureName = "";
+                savePictureName = name.getText() + "";
+                custom.cancel();
+                makeDialogSave().show();
+            }
+        });
+        return custom;
+    }
+
+
+    private void saveImage(Bitmap bitmap, String folder, String name) {
+        String ex_storage = Environment.getExternalStorageDirectory().getAbsolutePath();
+        // String folder_name = "/DCIM/Camera/"+folder+"/"; -> 기존 Camera에서 저장되는경우 화면에 보이질 않는다
+        String folder_name = "/Pictures/" + folder + "/"; // 새로운 앨범에 추가
+        String file_name = name + ".jpg";
+        String folder_path = ex_storage + folder_name;
+
+        try {
+            // 폴더 만들기
+            File savedFolder = new File(folder_path);
+            if (savedFolder != null && !savedFolder.exists()) {
+                Log.d("jys", "make folder / " + savedFolder.exists());
+                savedFolder.mkdirs();
+            } else {
+                Log.d("jys", "exists folder" + savedFolder.exists());
+            }
+            Log.d("jys", folder_path + file_name);
+
+            FileOutputStream out = new FileOutputStream(folder_path + file_name);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.close();
+
+            //미디어 스캐닝을 실시한다(갤러리 바로 보여지게)
+            Intent intent = new Intent();
+            sendBroadcast(new Intent(intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + folder_path + file_name)));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        if (pressBackCount == 0) {
-            Toast.makeText(this, "진짜 종료하고 싶으면 한번 더눌러", Toast.LENGTH_SHORT).show();
-            pressBackCount++;
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    private void beginCrop(Uri source) {
-        Log.d("beginCrop", "start");
-        Uri destination = Uri.fromFile(new File(PictureActivity.this.getCacheDir(), "cropped"));
-        Crop.of(source, destination).asSquare().start(PictureActivity.this);
-    }
-
-    private void handleCrop(int resultCode, Intent result) {
-        if (resultCode == Activity.RESULT_OK) {
-            Log.d("handleCrop", "Result_OK");
-            apImageview.setImageURI(Crop.getOutput(result));
-        } else if (resultCode == Crop.RESULT_ERROR) {
-            Log.d("handleCrop", "RESULT_ERROR");
-            Toast.makeText(PictureActivity.this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
 }
