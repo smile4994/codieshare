f78525b705b38ecc2ebf81491664db07beb819b9
smile4994 <30884887+smile4994@users.noreply.github.com>
2018-01-16 11:04:24 +0900

    up
    up

--- app/src/main/java/com/example/student/codieshare/Board.java
+++ app/src/main/java/com/example/student/codieshare/Board.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-import com.soundcloud.android.crop.Crop;
 
 /**
  * Created by student on 2018-01-09.
--- app/src/main/java/com/example/student/codieshare/Picture.java
+++ app/src/main/java/com/example/student/codieshare/Picture.java
@@ -14,14 +14,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.soundcloud.android.crop.Crop;
 import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
